40f97ae5f879e8a15cc76d8dcc1aaf36cd1761ce
daankent <daankentrop@gmail.com>
2026-08-08 11:34:30 +0200

    Configure PostgreSQL datasource and update build dependencies

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
 	kotlin("jvm") version "2.3.21"
+	kotlin("plugin.jpa") version "2.3.21"
 	kotlin("plugin.spring") version "2.3.21"
 	id("org.springframework.boot") version "4.1.0"
 	id("io.spring.dependency-management") version "1.1.7"
@@ -19,10 +20,19 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	testImplementation("org.springframework.boot:spring-boot-starter-security-test")
+	implementation("org.springframework.boot:spring-boot-starter-webmvc")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.3")
+
+	runtimeOnly("org.postgresql:postgresql")
+
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testImplementation("org.springframework.boot:spring-boot-starter-security-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
+
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
